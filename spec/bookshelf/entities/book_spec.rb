061820eea9f407e1888f94a157932c3cc2b577b1
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2022-12-25 12:58:30 +0200

    adds books table and lists all books on the /books page

---
+++ spec/bookshelf/entities/book_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Book, type: :entity do
+  it 'can be initialized with attributes' do
+    book = Book.new(title: 'Refactoring', author: 'Martin Fowler')
+    expect(book.title).to eq('Refactoring')
+    expect(book.author).to eq('Martin Fowler')
+  end
+end
